653945a949955d0523c861b40a35ae9dfc489a49
L3b1n <binleo07@icloud.com>
2025-04-01 20:48:15 +0300

    Add task 8 of bioinformatics

---
+++ Bioinformatics/task 8/dir/run_parsing.py
@@ -0,0 +1,6 @@
+import os
+
+
+os.system(f"python3 binana_log_parser_mod.py --input_file=\"log.txt\" \
+                                            --out_txt=\"parser_out.txt\" \
+                                            --out_csv=\"parser_out.csv\"")
