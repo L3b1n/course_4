6ba03bd35e4d9099ba033e9d459c04a5d058d98c
L3b1n <binleo07@icloud.com>
2025-07-27 17:18:30 +0300

    Add code of Subpixel chapter

---
+++ Diploma/Diploma tex/codeShaders/Subpixel/comp.glsl
@@ -0,0 +1,12 @@
+if (component == 0) {
+    s = pixel.r;
+}
+else if (component == 1) {
+    s = pixel.g;
+}
+else if (component == 2) {
+    s = pixel.b;
+}
+else if (component == 3) {
+    s = pixel.a;
+}
